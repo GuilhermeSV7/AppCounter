fc849d5c50b4eae1646505fdc2c6e1ef67418eb4
Guilherme.Lima <72620448+GuilhermeSV7@users.noreply.github.com>
2024-09-26 20:59:30 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import HomeScreen from './src/screen/HomeScreen';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <HomeScreen />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+  },
+});
